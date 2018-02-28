894f75a8d7aabea60559c9d3c31ec92323b9eb9a
Oscar <oscar@dynamicdesignspulaski.com>
2018-02-28 10:34:54 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import './Assets/css/default.min.css';
 
+// components
+import Header from './components/headerComponent/header';
+import Footer from './components/footerComponent/footer';
+import Homepage from './components/pages/homePage';
+
+
+// includes
+import './Assets/css/default.min.css';
 
 class App extends Component {
 	render() {
 		return (
 		<div className="App">
 
-				Test from App.js
-        
+        <Header />
+
+        <Homepage />
+
+        <Footer />
+
       </div>
 		);
 	}
